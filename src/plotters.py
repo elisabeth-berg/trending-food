51e7caf44bae56361faca12505b94726e3e4d2fb
Liz Berg <elisabeth.berg921@gmail.com>
2018-02-18 16:40:55 -0800

    Move files to src folder

--- src/plotters.py
+++ src/plotters.py
@@ -25,18 +25,29 @@ def yearly_food_plot(df, food, path=None, save=True):
     dates = df.loc[[i for i in df.index if food_stem in df.loc[i, 'food_stems']], ['id', 'post_date']]
     dates.index = dates['post_date']
     dates['ones'] = np.ones(len(dates))
-    fig, ax = plt.subplots()
+    months = np.array(range(1, 13))
+    mo_counts = np.zeros((len(range(2009, 2018)), len(months)))
+    fig, ax = plt.subplots(figsize=(10, 5))
     for i, year in enumerate(range(2009, 2018)):
         year = str(year)
         counts2 = dates.resample('1M').sum()
         yearly_food = counts2[year].resample('1M').sum()
         yearly_food.fillna(0, inplace=True)
         yearly_food['mos'] = [date.month for date in yearly_food['ones'].index]
-        months = np.array(range(1, 13))
-        mo_counts = np.zeros_like(months)
-        mo_counts[yearly_food['mos'] -1] = yearly_food['ones']
-        ax.plot(months, mo_counts, color=plt.cm.copper(10 + i*30))
-        ax.set_title('Yearly Popularity of {}'.format(food))
-        ax.set_xlabel('Month')
+        mo_counts[i][yearly_food['mos'] -1] = yearly_food['ones']
+        ax.plot(months, mo_counts[i],
+                linewidth=2,
+                color=plt.cm.cool(10 + i*30),
+                alpha=0.3,
+                label=year)
+    avg_counts = np.mean(mo_counts, axis=0)
+    ax.plot(months, avg_counts, linewidth=4, color='blue')
+    ax.set_xlim([0, 15])
+    ax.set_xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+    ax.set_title('Average Yearly Popularity of {}'.format(food))
+    ax.set_xlabel('Month')
+    ax.legend()
     if save:
         fig.savefig(path)
