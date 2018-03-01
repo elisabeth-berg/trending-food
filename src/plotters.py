af7285954fa3551a0c9a6625c4f0aa9393633845
Liz Berg <elisabeth.berg921@gmail.com>
2018-02-28 19:11:05 -0800

    Adjust yearly_food_plot to compute percentages

--- src/plotters.py
+++ src/plotters.py
@@ -19,27 +19,38 @@ def time_food_plot(df, food, n_months, path=None, save=True):
         fig.savefig(path)
 
 
-def yearly_food_plot(df, food, path=None, save=True):
+def yearly_food_plot(df, food, path=None, save=False):
     porter = PorterStemmer()
     food_stem = porter.stem(food.lower())
     dates = df.loc[[i for i in df.index if food_stem in df.loc[i, 'food_stems']], ['id', 'post_date']]
     dates.index = dates['post_date']
-    dates['ones'] = np.ones(len(dates))
+    dates['food_counts'] = np.ones(len(dates))
+
+    dates_all = df[['id', 'post_date']]
+    dates_all.index = dates_all['post_date']
+    dates_all['total_counts'] = np.ones(len(dates_all))
+
     months = np.array(range(1, 13))
     mo_counts = np.zeros((len(range(2009, 2018)), len(months)))
+    mo_counts_all = np.zeros((len(range(2009, 2018)), len(months)))
+    
     fig, ax = plt.subplots(figsize=(10, 5))
+    
     for i, year in enumerate(range(2009, 2018)):
         year = str(year)
-        counts2 = dates.resample('1M').sum()
-        yearly_food = counts2[year].resample('1M').sum()
-        yearly_food.fillna(0, inplace=True)
-        yearly_food['mos'] = [date.month for date in yearly_food['ones'].index]
-        mo_counts[i][yearly_food['mos'] -1] = yearly_food['ones']
+        d1 = dates[year].resample('1M').sum()
+        d2 = dates_all[year].resample('1M').sum()
+        counts = d1[['food_counts']].join(d2[['total_counts']], how='left')
+        counts.fillna(0, inplace=True)
+        counts['percentage'] = counts['food_counts'] / counts['total_counts']
+        counts['mos'] = [date.month for date in counts['food_counts'].index]
+        mo_counts[i][counts['mos'] -1] = counts['percentage']
         ax.plot(months, mo_counts[i],
-                linewidth=2,
-                color=plt.cm.cool(10 + i*30),
-                alpha=0.3,
-                label=year)
+            linewidth=2,
+            color=plt.cm.cool(10 + i*30),
+            alpha=0.3,
+            label=year)
+             
     avg_counts = np.mean(mo_counts, axis=0)
     ax.plot(months, avg_counts, linewidth=4, color='blue')
     ax.set_xlim([0, 15])
